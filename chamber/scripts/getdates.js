7154e5a091271a5842c2a53310ecaf30a9e338ef
dpina1988 <pin17017@byui.edu>
2024-05-18 23:00:05 -0300

    Week04_updates

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,16 @@
+/*
+NOTES
+01 - NEVER USE "var" it has limited scope.
+*/
+/*const date = new Date(document.lastModified);
+const year = date.getFullYear();
+const hours = ('0' + date.getHours()).slice(-2);
+const minutes = ('0' + date.getMinutes()).slice(-2);
+const seconds = ('0' + date.getSeconds()).slice(-2);
+
+document.getElementById('copyrightAndYear').innerHTML = year
+document.getElementById('lastUpdatedDate').innerHTML = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${hours}:${minutes}:${seconds}`
+*/
+
+let text = document.lastModified;
+document.getElementById("lastUpdatedDate").innerHTML = text;
