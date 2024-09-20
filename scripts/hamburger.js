e634511b08b38482e63174405d06527c6c5fceca
dpina1988 <pin17017@byui.edu>
2024-09-20 02:02:03 -0300

    Changes_10

--- scripts/hamburger.js
+++
@@ -1,8 +0,0 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', function() {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
-
